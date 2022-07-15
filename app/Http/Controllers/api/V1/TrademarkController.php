c1b893d207436fea2ff5c20a97824bab44a10cc7
elpolakopolania <gustav0796@hotmail.com>
2022-07-15 16:35:31 -0500

    Cargar resource y modelos

--- app/Http/Controllers/api/V1/TrademarkController.php
+++ app/Http/Controllers/api/V1/TrademarkController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\api\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
+class TrademarkController extends Controller
 {
     /**
      * Display a listing of the resource.
