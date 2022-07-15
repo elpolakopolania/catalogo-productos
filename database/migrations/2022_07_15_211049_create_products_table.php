ae3c1104dbc3645d4035c989b9362088af3f0421
elpolakopolania <gustav0796@hotmail.com>
2022-07-15 16:25:39 -0500

    Inicializar proyecto

---
+++ database/migrations/2022_07_15_211049_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("size");
+            $table->text("observation");
+            $table->bigInteger("trademarks_id")->unsigned();
+            $table->integer("inventory_quantity");
+            $table->dateTime("boarding_date");
+
+            //Foreign key
+            $table->foreign('trademarks_id')->references('id')->on('trademarks');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
